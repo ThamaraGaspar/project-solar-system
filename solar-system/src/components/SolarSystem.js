9ef9ac9d3d5a553b8b889a7fd3f4a98f213f00a4
Thamara <silvagaspar92@gmail.com>
2022-05-08 10:25:10 -0300

    iniciando

---
+++ solar-system/src/components/SolarSystem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PlanetCard from './PlanetCard';
+import Title from './Title';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (<PlanetCard
+          key={ name }
+          planetName={ name }
+          PlanetImage={ image }
+        />))}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
